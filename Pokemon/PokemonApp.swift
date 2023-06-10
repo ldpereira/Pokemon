fa51b2d6902252d3b1c99c221f157d1c4483c801
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-11 03:48:03 +0530

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Lucas Pereira on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
